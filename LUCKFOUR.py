8122dcab27b0fc85f90bf54b775c0e6880da4424
Govind Krishna Lal Balaji <63466198+gvndkrishna@users.noreply.github.com>
2021-10-05 00:32:06 +0530

    Solution to LuckFour problem

---
+++ LUCKFOUR.py
@@ -0,0 +1,12 @@
+T=int(input())
+for i in range(T):
+    count=0
+    x=input()
+    for j in range(len(x)):
+        if(int(x[j])==4):
+           count+=1
+        else:
+            pass
+    print(count)
+              
+        
