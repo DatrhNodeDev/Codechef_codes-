79e30bbe56c4429f03e4c048fe84156eb12c8366
Chirag Saxena <64087682+ChiragSaxena5437@users.noreply.github.com>
2021-10-04 19:17:44 +0530

    Added Find_Second_Largest

---
+++ Find_Second_Largest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;int b;int x;int y;
+    cin>>a;cin>>b;cin>>x;cin>>y;
+
+    if(a>b && b>c && a>c)
+    {
+        cout<<
+    }
+    if(b>a && b>c && a>c)
+    {
+        
+    }
+    if(c>a && c>b && c>a)
+    {
+        
+    }
+}
